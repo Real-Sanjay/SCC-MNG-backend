38037966d986c9a45a2f91a60814fe24bd328b49
Niharika <20311a04f9@sreenidhi.edu.in>
2024-12-27 14:21:40 +0530

    start

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const trainerRoutes = require('./routes/trainerRoute');
 const programRoute = require('./routes/programRoute');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -29,3 +29,7 @@ mongoose
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+
+///git check
